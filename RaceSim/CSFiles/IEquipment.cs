087807096ce43026a0d2d049ad2315f1abf0e04e
Frepds <94062636+frepds@users.noreply.github.com>
2023-02-13 13:50:48 +0100

    Commit 2

--- RaceSim/CSFiles/IEquipment.cs
+++ RaceSim/CSFiles/IEquipment.cs
@@ -4,7 +4,26 @@ public interface IEquipment
 {
     static void Main(string[] args)
     {
-        string commit = "Initial Commit";
-        Console.WriteLine($"RaceSim - {commit}");
+        int[] amount = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < amount.Length; i++)
+        {
+            Console.WriteLine(amount[i]);
+        }
+        Console.WriteLine("Amount enter");
+        int amountEnter = Convert.ToInt32(Console.ReadLine());
+        int j = 1;
+        for (int i = 0; i < amountEnter; i++)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+        Console.WriteLine("Press any key to exit.");
     }
 }
