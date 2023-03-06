6401084bd33aaff02927dcb6ab46d934da700f71
frepds <94062636+frepds@users.noreply.github.com>
2023-03-06 20:50:29 +0100

    Commit BAI Aftekenopdracht1

--- BAI-Aftekenopdracht1/Aftekenopdracht1.cs
+++ BAI-Aftekenopdracht1/Aftekenopdracht1.cs
@@ -5,7 +5,45 @@ public class Aftekenopdracht1
     public static void Opg1FilterList(List<int> lijst)
     {
         Dictionary<int, int> dictionary = new Dictionary<int, int>();
-        
+        var i = lijst.Count;
+        var j = 0;
+
+        // Fill
+        while (i > 0)
+        {
+            dictionary.Add(j, lijst[j]);
+            j++; i--;
+        }
+
+        // Remove
+        while (i > 0)
+        {
+            var num = lijst[j];
+            var count = 0;
+            while (i > 0)
+            {
+                if (lijst[j].Equals(num))
+                {
+                    count++;
+                }
+                j++; i--;
+            }
+            
+            
+            if (dictionary.ContainsValue(lijst[j]))
+            {
+                
+            }
+            
+            
+            // if (j > 0 && dictionary.ContainsValue(lijst[j]))
+            // {
+            //     Console.Write(lijst[j] + " ");
+            // }
+            // dictionary.Add(j, lijst[j]);
+            j++; i--;
+        }
+
     }
 
     public static Queue<int> Opdr2AQueue50()
@@ -24,8 +62,8 @@ public class Aftekenopdracht1
         Stack<int> tijdelijkeStack = new Stack<int>();
         while (q.Count > 0)
         {
-            int value = q.Dequeue();
-            if ((value % 4) == 0)
+            var value = q.Dequeue();
+            if (value % 4 == 0)
             {
                 tijdelijkeStack.Push(value);
             }
@@ -36,7 +74,7 @@ public class Aftekenopdracht1
     public static Stack<int> Opdr3RandomNumbers(int lower, int upper, int count)
     { 
         var r = new Random();
-        int counter = 0;
+        var counter = 0;
 
         Stack<int> s = new Stack<int>();
 
@@ -55,13 +93,11 @@ public class Aftekenopdracht1
                         run = false;
                     }
                 } while (run);
-                Console.WriteLine("[CONTAINS] " + (counter + 1) + ": " + generated);
             }
             else
             {
                 s.Push(generated);
             }
-            //Console.WriteLine((counter + 1) + ": " + generated);
             counter++;
         } while (counter < count);
         return s;
--- BAI-Aftekenopdracht1/Program.cs
+++ BAI-Aftekenopdracht1/Program.cs
@@ -4,24 +4,13 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<int, int> dictionary = new Dictionary<int, int>();
-        dictionary.Add(1,1);
-        dictionary.Add(2,1);
-        dictionary.Add(3,2);
-
-        Console.WriteLine();
-        
-        foreach (var i in dictionary)
-        {
-            Console.WriteLine(i.Value);
-        }
-
-        
+        // Opgave 1
+        Console.WriteLine("Opgave 1");
         List<int> list = new List<int> { 1, 3, 5, 7, 3, 8, 9, 5 };
         Aftekenopdracht1.Opg1FilterList(list);
 
         // Opgave 2
-        Console.WriteLine("Opgave 2:");
+        Console.WriteLine("\nOpgave 2:");
         Queue<int> queue = Aftekenopdracht1.Opdr2AQueue50();
         Stack<int> stack = Aftekenopdracht1.Opdr2BStackFromQueue(queue);
         foreach (var s in stack)
@@ -37,9 +26,6 @@ public class Program
         // Stack 2
         Stack<int> stackExample2 = Aftekenopdracht1.Opdr3RandomNumbers(10, 15, 6);
         foreach (var i in stackExample2) { Console.Write(i + " "); } Console.WriteLine();
-
-       
-        
         
         Console.ReadKey();
     }
