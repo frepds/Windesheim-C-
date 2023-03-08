344d65119b3039bd6e46c72e8335c769b7396df4
frepds <94062636+frepds@users.noreply.github.com>
2023-03-08 20:57:51 +0100

    Commit les 5B

---
+++ Les5B/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Les5B;
+
+public class Program
+{
+    public Program()
+    {
+        Go go = Lopen;
+        go();
+    }
+
+    public delegate void Go();
+
+    public static void Lopen()
+    {
+        Console.WriteLine("Hello world");
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+    }
+}
---
+++ RaceSim/Controller/Data.cs
@@ -0,0 +1,13 @@
+﻿namespace RaceSim;
+
+public class Data
+{
+    public static Competition Competition { get; set; }
+    
+    
+    static void AddMethod()
+    {
+        
+    }
+    
+}
