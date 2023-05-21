542d8d2ac0d6a78a6f7e2d9ba026fbd97d83971f
frepds <94062636+frepds@users.noreply.github.com>
2023-05-21 21:53:41 +0200

    Commit C#

---
+++ Audio/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.IO;
+using System.Reflection;
+
+string contentRootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+string audioFilePath = Path.Combine(contentRootPath, "Duim.wav");
+
+Console.WriteLine($"Path: {audioFilePath}");
--- TestMain/Class1.cs
+++ TestMain/Class1.cs
@@ -1,79 +1,58 @@
 ﻿namespace TestMain;
-using Newtonsoft.Json;
 
 public class Class1
 {
     private const int TotalQuestions = 12;
-    private static readonly Dictionary<int, string> QuestionDictionary = new(TotalQuestions);
-    private static readonly Dictionary<int, string> AnswerDictionary = new(TotalQuestions);
+
+    private static readonly Dictionary<int, Tuple<string, string>> QuestionDictionary = new(TotalQuestions);
+    private static readonly Dictionary<int, Tuple<string, string>> AnswerDictionary = new(TotalQuestions);
     
-    private static void FillDictionaryQuestions(int n, int g)
+    private static void FillDictionaryQuestions(int Number, int TypeSound, int AudioSound)
     {
-        switch (g)
-        { 
-            case 0: 
-                QuestionDictionary.Add(n, "Duim"); 
-                break;
-            case 1: 
-                QuestionDictionary.Add(n, "Geit"); 
-                break;
-            case 2: 
-                QuestionDictionary.Add(n, "Kip"); 
-                break;
-            case 3: 
-                QuestionDictionary.Add(n, "Leeuw"); 
-                break;
-            case 4: 
-                QuestionDictionary.Add(n, "Kat"); 
-                break;
-            case 5: 
-                QuestionDictionary.Add(n, "Rat"); 
-                break;
-            case 6: 
-                QuestionDictionary.Add(n, "Vuur"); 
-                break;
-            case 7: 
-                QuestionDictionary.Add(n, "Zaag"); 
-                break;
-            case 8: 
-                QuestionDictionary.Add(n, "Wiel"); 
-                break;
-            
-        }
+        var Sound = TypeSound switch
+        {
+            0 => "Mono-Left",
+            1 => "Mono-Right",
+            2 => "Stereo",
+        };
+
+        var Audio = AudioSound switch
+        {
+            0 => "Duim",
+            1 => "Geit",
+            2 => "Kip",
+            3 => "Leeuw",
+            4 => "Kat",
+            5 => "Rat",
+            6 => "Vuur",
+            7 => "Zaag",
+            8 => "Wiel",
+        };
+
+        var Inner = new Tuple<string, string>(Sound, Audio);
+        QuestionDictionary.Add(Number, Inner);
     }
     
-    private static void FillDictionaryAnswers(int n, int g)
+    private static void FillDictionaryAnswers(int Number, int AudioSound)
     {
-        switch (g)
-        { 
-            case 0: 
-                AnswerDictionary.Add(n, "Duim"); 
-                break;
-            case 1: 
-                AnswerDictionary.Add(n, "Geit"); 
-                break;
-            case 2: 
-                AnswerDictionary.Add(n, "Kip"); 
-                break;
-            case 3: 
-                AnswerDictionary.Add(n, "Leeuw"); 
-                break;
-            case 4: 
-                AnswerDictionary.Add(n, "Kat"); 
-                break;
-            case 5: 
-                AnswerDictionary.Add(n, "Rat"); 
-                break;
-            case 6: 
-                AnswerDictionary.Add(n, "Vuur"); 
-                break;
-            case 7: 
-                AnswerDictionary.Add(n, "Zaag"); 
-                break;
-            case 8: 
-                AnswerDictionary.Add(n, "Wiel"); 
-                break;
-        }
+        var InnerTuple = QuestionDictionary[Number];
+        var Key = InnerTuple.Item1;
+
+        var Audio = AudioSound switch
+        {
+            0 => "Duim",
+            1 => "Geit",
+            2 => "Kip",
+            3 => "Leeuw",
+            4 => "Kat",
+            5 => "Rat",
+            6 => "Vuur",
+            7 => "Zaag",
+            8 => "Wiel",
+        };
+        
+        var Inner = new Tuple<string, string>(Key, Audio);
+        AnswerDictionary.Add(Number, Inner);
     }
     
     
@@ -81,22 +60,40 @@ public class Class1
     public static void Main()
     {
         var random = new Random();
-        const int n = 12;
-        for (var i = 0; i < n; i++)
+
+        for (var i = 0; i < TotalQuestions; i++)
         {
             var r1 = random.Next(9);
-            FillDictionaryQuestions(i, r1);
-            var r2 = random.Next(9);
-            FillDictionaryAnswers(i, r2);
+            var r2 = random.Next(3);
+            FillDictionaryQuestions(i, r2, r1);
         }
         
-        for (var i = 0; i < n; i++)
+        for (var i = 0; i < TotalQuestions; i++)
         {
-            Console.Write($"Antwoord [{QuestionDictionary.Keys.ElementAt(i)}: {QuestionDictionary.Values.ElementAt(i)}] " +
-                          "| " +
-                          $"Gebruiker [{AnswerDictionary.Keys.ElementAt(i)}: {AnswerDictionary.Values.ElementAt(i)}] - ");
-            Console.Write(QuestionDictionary.Values.ElementAt(i) == AnswerDictionary.Values.ElementAt(i));
-            Console.WriteLine();
+            var r1 = random.Next(9);
+            FillDictionaryAnswers(i, r1);
         }
+        
+        
+        var output1 = string.Join(", \n", QuestionDictionary.Select(entry => $"Key: {entry.Key}, Value1: {entry.Value.Item1}, Value2: {entry.Value.Item2}"));
+        Console.WriteLine(output1);
+        
+        Console.WriteLine("\nAnswers:\n");
+        
+        var output2 = string.Join(", \n", AnswerDictionary.Select(entry => $"Key: {entry.Key}, Value1: {entry.Value.Item1}, Value2: {entry.Value.Item2}"));
+        Console.WriteLine(output2);
+
+        Console.WriteLine("\n");
+        
+        for (int i = 0; i < TotalQuestions; i++)
+        {
+            var questionString2 = QuestionDictionary[i].Item2;
+            var answerString2 = AnswerDictionary[i].Item2;
+
+            Console.WriteLine(questionString2 == answerString2
+                ? $"Question {i + 1}: String2 values match {questionString2} - {answerString2}"
+                : $"Question {i + 1}: String2 values do not match {questionString2} - {answerString2}");
+        }
+       
     }
 }
